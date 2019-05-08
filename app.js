d3b43f270fb9cdd09ec14f908a86bfca9bc858a6
Josh Kuhar <jdkuhar@gmail.com>
2019-05-08 01:46:54 -0400

    added $emit

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
-let data = {
-    name: 'billy bob',
-    foos: [
-      {id: 1, text: 'hahahaha cough'},
-      {id: 2, text: 'bip bip bip'},
-      {id: 3, text: 'bop bop bop'}
-    ]
-}
 
-var todoComp = Vue.component('todo-item', {
-    template: '<li>{{todo.text}}</li>',
-    props: ['todo']
+Vue.component('todo-item', {
+    template: `<li>{{todo.text}} <button v-on:click="$emit('delete-item')">{{buttonText}}</button></li>`,
+    props: ['todo'],
+    data: function () {
+        return {
+            buttonText: 'delete'
+        }
+    }
 })
 
 var app = new Vue({
   el: '#app',
   data: {
-    todoList: data.foos,
-    name: data.name,
+    todoList: [],
     todoItem: '',
-    message: ''
+    id: 0
   },
   methods: {
     addItemToList: function () {
-        this.todoList.push({text: 'oh my gawd'})
+        this.id++
+        this.todoList.push({id: this.id, text: this.todoItem})
+        this.todoItem = ''
     },
-    deleteItem: function() {
-        let newTodoList = this.todoList.map(function(i){
-            return i
-        })
-        newTodoList.pop()
-        this.todoList = newTodoList    
+    deleteItem: function (itemId) {
+        this.todoList = this.todoList.filter( i => i.id !== itemId )
     }
-  },
-  components: {
-    todoComp: todoComp
   }
 })
+
 
